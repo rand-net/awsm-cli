78d7d5fa78199d821eca68583e51d72bda47e3c6
rant-net <34341872+rand-net@users.noreply.github.com>
2021-07-22 17:19:39 +0530

    Bumps to version 0.0.2

--- awsm_cli/__init__.py
+++ awsm_cli/__init__.py
@@ -2,7 +2,7 @@ from .awsm_cli import *
 import argparse
 import sys
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 def main(argv=None):
@@ -16,15 +16,15 @@ def main(argv=None):
     args = argparser.parse_args()
 
     if args.browser:
-        awsm_cli = Awsm_cli()
-        selected_domain = awsm_cli.select_domain()
-        domain_topic_repo = awsm_cli.select_domain_topic_repo(selected_domain)
-        selected_item_url = awsm_cli.select_awesome_item(domain_topic_repo)
-        awsm_cli.open_browser(selected_item_url, args.browser)
+        awsm_cli = AwsmCLI(args.browser)
+        selected_domain = awsm_cli.domain_prompt()
+        domain_topic_repo = awsm_cli.topic_prompt()
+        selected_item_url = awsm_cli.item_prompt()
+        awsm_cli.open_item_browser()
 
     else:
-        awsm_cli = Awsm_cli()
-        selected_domain = awsm_cli.select_domain()
-        domain_topic_repo = awsm_cli.select_domain_topic_repo(selected_domain)
-        selected_item_url = awsm_cli.select_awesome_item(domain_topic_repo)
-        awsm_cli.open_browser(selected_item_url)
+        awsm_cli = AwsmCLI(args.browser)
+        selected_domain = awsm_cli.domain_prompt()
+        domain_topic_repo = awsm_cli.topic_prompt()
+        selected_item_url = awsm_cli.item_prompt()
+        awsm_cli.open_item_browser()
--- awsm_cli/awsm_cli.py
+++ awsm_cli/awsm_cli.py
@@ -1,112 +1,100 @@
-from art import *
+from art import tprint
 import requests
 import subprocess
 import webbrowser
 from prompt_toolkit import prompt
-from subprocess import PIPE, run
-from prompt_toolkit.completion import (
-    WordCompleter,
-    Completer,
-    FuzzyWordCompleter,
-)
+from prompt_toolkit.completion import FuzzyWordCompleter
 
 
-class Awsm_cli:
-    def __init__(self):
-        # https://raw.githubusercontent.com/lockys/Awesome.json/master/awesome/awesome.json
+class AwsmCLI:
+    """Awesome CLI
+    CLI to access awesome lists
+
+    JSON Data URLS
+    # https://raw.githubusercontent.com/lockys/Awesome.json/master/awesome/awesome.json
+    # https://raw.githubusercontent.com/lockys/Awesome.json/master/repo-json/vinta-awesome-python.json
+    """
+
+    def __init__(self, browser=None):
         tprint("AWSM-CLI")
         print("Downloading Domain List...\n")
         awesome_list_source = requests.get(
             "https://raw.githubusercontent.com/lockys/Awesome.json/master/awesome/awesome.json"
         )
+        self.browser = browser
         self.awesome_list = awesome_list_source.json()
-
-    def select_domain(self):
-        """Returns the selected domain
-        return: selected_domain
-        """
-        domains = []
-
-        for key, value in self.awesome_list.items():
-            domains.append(key)
-
-        domains_completer = FuzzyWordCompleter(domains)
-        selected_domain = prompt("Domain: ", completer=domains_completer)
-        return selected_domain
-
-    def select_domain_topic_repo(self, selected_domain):
-        """Returns the selected domain topic repo
-        return: domain_topic_repo
-        """
-
-        # Get the list for chosen domain
-        for key, value in self.awesome_list.items():
-            if key == selected_domain:
-                selected_domain_topics_list = value
-
-        # Build the domain topics list
-        selected_domain_topics = []
-        selected_domain_topic_repos = []
-        for value in selected_domain_topics_list:
-            selected_domain_topic_repos.append(value["repo"])
-            selected_domain_topics.append(value["name"])
-
-        domain_topics_completer = FuzzyWordCompleter(selected_domain_topics)
-        selected_topic = prompt("Topic: ", completer=domain_topics_completer)
-        selected_topic_index = selected_domain_topics.index(selected_topic)
-
-        domain_topic_repo = selected_domain_topic_repos[selected_topic_index].replace(
-            "/", "-"
-        )
-        return domain_topic_repo
-
-    def select_awesome_item(self, selected_domain_topic_repo):
-        """ Returns the selected item URL
-        return: selected_item_url
-        """
-
-        # https://raw.githubusercontent.com/lockys/Awesome.json/master/repo-json/vinta-awesome-python.json
-        awesome_repo_url = (
+        self.selected_domain = ""
+        self.selected_topic = ""
+        self.selected_topic_repo = ""
+        self.topic_repo_json_url = (
             "https://raw.githubusercontent.com/lockys/Awesome.json/master/repo-json/"
-            + selected_domain_topic_repo
-            + ".json"
         )
+        self.selcted_item_url = ""
+
+    def domain_prompt(self):
+        """
+        Prompt for domain selection
+        """
+        domains = [key for key, value in self.awesome_list.items()]
+        domains_completer = FuzzyWordCompleter(domains)
+        self.selected_domain = prompt("Domain: ", completer=domains_completer)
+
+    def topic_prompt(self):
+        """
+        Prompt for topic selection
+        """
+        # Topics for chosen domain
+        for key, value in self.awesome_list.items():
+            if key == self.selected_domain:
+                selected_topics_listdict = value
+
+        # Domain Prompt
+        selected_topics = [value["name"] for value in selected_topics_listdict]
+        domain_topics_completer = FuzzyWordCompleter(selected_topics)
+        selected_topic = prompt("Topic: ", completer=domain_topics_completer)
+
+        selected_topic_index = selected_topics.index(selected_topic)
+        self.selected_topic_repo = selected_topics_listdict[selected_topic_index][
+            "repo"
+        ].replace("/", "-")
+
+    def item_prompt(self):
+        """
+        Prompt for item selection
+        """
+
+        # Get JSON data about repo
+        awesome_repo_url = self.topic_repo_json_url + self.selected_topic_repo + ".json"
         awesome_repo_source = requests.get(awesome_repo_url)
         awesome_repo_json = awesome_repo_source.json()
 
-        item_names = []
-        item_descriptions = []
         item_urls = []
         item_names_descriptions = []
         for item_info in awesome_repo_json:
-            item_names.append(item_info["name"])
-
+            item_urls.append(item_info["url"])
             try:
-                item_descriptions.append(item_info["description"])
                 item_names_descriptions.append(
                     item_info["name"] + " -> " + item_info["description"]
                 )
-            except:
-                item_descriptions.append("None")
+            except KeyError:
                 item_names_descriptions.append(
                     item_info["name"] + " -> " + "No Description"
                 )
 
-            item_urls.append(item_info["url"])
-
         item_completion_list = FuzzyWordCompleter(item_names_descriptions)
         selected_item = prompt("Item: ", completer=item_completion_list)
         selected_item_index = item_names_descriptions.index(selected_item)
-        selected_item_url = item_urls[selected_item_index]
+        self.selected_item_url = item_urls[selected_item_index]
         print("\n" + selected_item + "\n")
-        return selected_item_url
 
-    def open_browser(self, url, browser=None):
-        """Opens the url in given browser"""
-        if not browser:
-            webbrowser.open(url)
+    def open_item_browser(self):
+        """
+        Opens the item url in the given browser
+        """
+        if not self.browser:
+            webbrowser.open(self.selected_item_url)
         else:
-            command = browser + " " + url
+            command = self.browser + " " + self.selected_item_url
             process = subprocess.Popen(
                 command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
             )
